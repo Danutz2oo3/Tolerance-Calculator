d750b0f71888028a2d5e24ac84bd8bc9dda733e0
strungardan <dan.strungar@guehring.de>
2023-07-17 09:28:42 +0200

    initial commit

---
+++ src/main/java/de/guehring/tolerancecalculator/ToleranceCalculatorApplication.java
@@ -0,0 +1,13 @@
+package de.guehring.tolerancecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToleranceCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToleranceCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/guehring/tolerancecalculator/ToleranceCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.guehring.tolerancecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToleranceCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
